2a602f59cf5bf173b47e3d0e0b43ba969e9e13d0
piqxxu <piquradya@gmail.com>
2025-03-13 11:09:45 +0700

    Menambahkan class FanLaptop

--- Program.cs
+++ Program.cs
@@ -1,39 +1,132 @@
-﻿ class kodeProduk
-{ 
-    private static Dictionary<string, string> kodeProduk2 = new Dictionary<string, string>()
-    {
-        {"Laptop", "E100"},
-        {"Smartphone", "E101"},
-        {"Tablet", "E102"},
-        {"Headset", "E103"},
-        {"Keyboard", "E104"},
-        {"Mouse", "E105"},
-        {"Printer", "E106"},
-        {"Monitor","E107" },
-        {"Smartwatch","E108" },
-        {"Kamera", "E109" }
-    };
+﻿// class kodeProduk
+//{ 
+//    private static Dictionary<string, string> kodeProduk2 = new Dictionary<string, string>()
+//    {
+//        {"Laptop", "E100"},
+//        {"Smartphone", "E101"},
+//        {"Tablet", "E102"},
+//        {"Headset", "E103"},
+//        {"Keyboard", "E104"},
+//        {"Mouse", "E105"},
+//        {"Printer", "E106"},
+//        {"Monitor","E107" },
+//        {"Smartwatch","E108" },
+//        {"Kamera", "E109" }
+//    };
 
-    public static string getKodeProduk(string produk)
+//    public static string getKodeProduk(string produk)
+//    {
+//        if (kodeProduk2.ContainsKey(produk))
+//        {
+//            return kodeProduk2[produk];
+//        } else
+//        {
+//            return "Produk tidak ditemukan";
+//        }
+//    }
+//}
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        Console.Write("Masukkan nama produk: ");
+//        string produk = Console.ReadLine();
+
+//        string kodeProdukk = kodeProduk.getKodeProduk(produk);
+//        Console.WriteLine($"Kode produk: { kodeProdukk}");
+//    }
+//}
+
+class fanLaptop
+{
+    private enum state { Performance, Quiet, Turbo, Balanced };
+    private state currState;
+
+    public fanLaptop()
     {
-        if (kodeProduk2.ContainsKey(produk))
+        currState = state.Quiet;
+        Console.WriteLine("Fan dalam kondisi Quite Mode");
+    }
+
+    public void QuietMode()
+    {
+        if (currState == state.Quiet)
         {
-            return kodeProduk2[produk];
-        } else
+            Console.WriteLine("\nFan telah masuk kedalam kondisi Quiet Mode");
+        }
+        else
         {
-            return "Produk tidak ditemukan";
+            currState = state.Quiet;
+            Console.WriteLine("\nFan dalam kondisi Quite Mode");
+
         }
     }
+
+    public void TurboMode()
+    {
+        if (currState == state.Turbo)
+        {
+            Console.WriteLine("\nFan Quiet berubah menjadi Turbo");
+        }
+        else
+        {
+            currState = state.Performance;
+            Console.WriteLine("\nFan dalam kondisi Turbo Mode");
+
+        }
+    }
+    public void balancedMode()
+    {
+        if (currState == state.Balanced)
+        {
+            Console.WriteLine("\n“Fan Quiet berubah menjadi Balanced");
+        }
+        else
+        {
+            currState = state.Balanced;
+            Console.WriteLine("\nFan dalam kondisi Balanced Mode");
+
+        }
+    }
+    public void performanceMode()
+    {
+        if (currState == state.Performance)
+        {
+            Console.WriteLine("\n“Fan Quiet berubah menjadi Performance");
+        }
+        else
+        {
+            currState = state.Performance;
+            Console.WriteLine("\nFan dalam kondisi Performance Mode");
+
+        }
+    }
+
 }
 
 class Program
 {
     static void Main()
     {
-        Console.Write("Masukkan nama produk: ");
-        string produk = Console.ReadLine();
+        fanLaptop fan = new fanLaptop();
 
-        string kodeProdukk = kodeProduk.getKodeProduk(produk);
-        Console.WriteLine($"Kode produk: { kodeProdukk}");
+        while (true)
+        {
+            Console.Write("Input: ");
+            string command = Console.ReadLine().ToLower();
+
+            if (command == "quiet")
+                fan.QuietMode();
+            else if (command == "turbo")
+                fan.TurboMode();
+            else if (command == "performance")
+                fan.performanceMode();
+            else if (command == "balanced")
+                fan.balancedMode();
+            else
+                Console.WriteLine("Input tidak sesuai");
+        }
     }
 }
+
--- obj/Debug/net8.0/Modul4_103022300023.AssemblyInfo.cs
+++ obj/Debug/net8.0/Modul4_103022300023.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Modul4_103022300023")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f822b7b9a8e0bc0bf330d82ea3d178fcfdfb86c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+add9795b7b94592ca7a49605179e1aadadee1985")]
 [assembly: System.Reflection.AssemblyProductAttribute("Modul4_103022300023")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Modul4_103022300023")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
